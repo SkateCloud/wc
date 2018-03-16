9bc7203b62f9f084aced0c59b35cc110f6f34483
白云 <baiyun_a@yahoo.com>
2018-03-16 20:24:41 +0800

    Update

--- src/UnitTest.java
+++ src/UnitTest.java
@@ -1,4 +1,124 @@
-package PACKAGE_NAME;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 public class UnitTest {
+    // test -c
+    public static void main(String[] args) throws IOException {
+        // 基本功能测试
+        // 测试-c
+        test_c("abcd fgh", "wordcounttest.txt 字符数: 8\n");
+        test_c("","wordcounttest.txt 字符数: 0\n");
+        // 测试-w
+        test_w("hello world","wordcounttest.txt 单词数: 2\n");
+        test_w("", "wordcounttest.txt 单词数: 0\n");
+        // 测试-l
+        test_l("hello\nworld\n!","wordcounttest.txt 行数: 3\n");
+        test_l("","wordcounttest.txt 行数: 1\n");
+        // 测试-o
+        FileWriter fout = new FileWriter("wordcounttest.txt");
+        fout.append("hello\nworld\n!!!");
+        fout.close();
+        WordCount.main(new String[] {"-l","wordcounttest.txt","-o","word2.txt"});
+        String content = readFile("word2.txt");
+        String answer = "wordcounttest.txt 行数: 3\n";
+        if (Objects.equals(content, answer)) {
+            System.out.println("-o test : " + " YES");
+        } else {
+            System.out.println("-o test : " + " NO");
+        }
+        // 测试多参数
+        FileWriter fout_1 = new FileWriter("wordcounttest.txt");
+        fout_1.append("hello\nworld\n!!!");
+        fout_1.close();
+        WordCount.main(new String[] {"-c","w","-l","wordcounttest.txt","-o","word2.txt"});
+        String content_1 = readFile("word2.txt");
+        String answer_1 = "wordcounttest.txt 行数: 3\n";
+        if (Objects.equals(content_1, answer_1)) {
+            System.out.println("basic test : " + " YES");
+        } else {
+            System.out.println("basic test : " + " NO");
+        }
+
+        // 扩展功能测试
+        //-a
+        FileWriter fout_2 = new FileWriter("wordcounttest.txt");
+        fout_2.append("hello\nworld\n!!!");
+        fout_2.close();
+        WordCount.main(new String[] {"-a","wordcounttest.txt"});
+        String content_2 = readFile("result.txt");
+        String answer_2 = "wordcounttest.txt 代码行/空行/注释行: 3/0/0\n";
+        if (Objects.equals(content_2, answer_2)) {
+            System.out.println("-a test : " + " YES");
+        } else {
+            System.out.println("-a test : " + " NO");
+        }
+        // -s
+        WordCount.main(new String[] {"-s","-a","*.txt"});
+        String content_3 = readFile("result.txt");
+        String answer_3 = "result.txt 代码行/空行/注释行: 1/0/0\nstop.txt 代码行/空行/注释行: 1/0/0\nwordcounttest.txt 代码行/空行/注释行: 3/0/0\nword2.txt 代码行/空行/注释行: 1/0/0\n";
+        if (Objects.equals(content_3, answer_3)) {
+            System.out.println("-s test : " + " YES");
+        } else {
+            System.out.println("-s test : " + " NO");
+        }
+        // -e
+        FileWriter fout_4 = new FileWriter("wordcounttest.txt");
+        fout_4.append("hello world !!! hello there");
+        fout_4.close();
+        WordCount.main(new String[] {"-w","wordcounttest.txt","-e","stop.txt"});
+        String content_4 = readFile("result.txt");
+        String answer_4 = "wordcounttest.txt 单词数: 3\n";
+        if (Objects.equals(content_4, answer_4)) {
+            System.out.println("-e test : " + " YES");
+        } else {
+            System.out.println("-e test : " + " NO");
+        }
+    }
+    private static void test_c(String input, String answer) throws IOException {
+        FileWriter fout = new FileWriter("wordcounttest.txt");
+        fout.append(input);
+        fout.close();
+        WordCount.main(new String[] {"-c","wordcounttest.txt"});
+        String content = readFile("result.txt");
+        if (Objects.equals(content, answer)) {
+            System.out.println("-c test : " + " YES");
+        } else {
+            System.out.println("-c test : " + " NO");
+        }
+    }
+    private static void test_w(String input, String answer) throws IOException {
+        FileWriter fout = new FileWriter("wordcounttest.txt");
+        fout.append(input);
+        fout.close();
+        WordCount.main(new String[] {"-w","wordcounttest.txt"});
+        String content = readFile("result.txt");
+        if (Objects.equals(content, answer)) {
+            System.out.println("-w test : "+" YES");
+        } else {
+            System.out.println("-w test : " + " NO");
+        }
+    }
+    private static void test_l(String input, String answer) throws IOException {
+        FileWriter fout = new FileWriter("wordcounttest.txt");
+        fout.append(input);
+        fout.close();
+        WordCount.main(new String[] {"-l","wordcounttest.txt"});
+        String content = readFile("result.txt");
+        if (content.equals(answer)) {
+            System.out.println("-l test : " + " YES");
+        } else {
+            System.out.println("-l test : " + " NO");
+        }
+    }
+    private static String readFile(String path) { // 读文件
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(path));
+            return new String(encoded, "UTF-8");
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
--- src/WordCount.java
+++ src/WordCount.java
@@ -1,14 +1,15 @@
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class WordCount {
 
-    class Arg {
+    class Arg { // 解析参数列表所需类
         public char flag;
         public String content;
 
@@ -20,15 +21,16 @@ public class WordCount {
         public boolean isCWL() {
             return "cwl".indexOf(flag) != -1;
         }
+
     }
 
     public static void main(String[] args) {
         new WordCount().start(args);
     }
 
-    private final List<Arg> argList = new ArrayList<>();
+    private final List<Arg> argList = new ArrayList<>(); // 解析后的列表
 
-    private void start(String[] args) {
+    private void start(String[] args) { // 主函数
         parseArgs(args);
 
         StringBuilder result = new StringBuilder();
@@ -50,27 +52,65 @@ public class WordCount {
             }
         }
 
+        boolean recursive = findArg('s') != null;
+
         {
             Arg arg = findArg('c');
             if (arg != null) {
-                String content = readFile(arg.content);
-                result.append("Character count: ").append(content.length()).append('\n');
+                for (String path : findFiles(arg.content, recursive)) {
+                    String content = readFile(path);
+                    result.append(path).append(" 字符数: ").append(content.length()).append('\n');
+                }
             }
         }
         {
             Arg arg = findArg('w');
+            Arg arg1 = findArg('e');
+
+            Set<String> blacklist = new HashSet<>();
+            if (arg1 != null) {
+                blacklist.addAll(Arrays.asList(readFile(arg1.content).split(" ")));
+            }
+
             if (arg != null) {
-                String content = readFile(arg.content);
-                int words = content.split(" |\\n").length;
-                result.append("Word count: ").append(words).append('\n');
+                for (String path : findFiles(arg.content, recursive)) {
+                    String content = readFile(path);
+                    int words = (int) Arrays.stream(content.split(" |\\n"))
+                            .filter(it -> !blacklist.contains(it))
+                            .filter(it -> !it.trim().isEmpty())
+                            .count();
+                    result.append(path).append(" 单词数: ").append(words).append('\n');
+                }
             }
         }
         {
             Arg arg = findArg('l');
             if (arg != null) {
-                String content = readFile(arg.content);
-                int lines = 1 + (int) content.chars().filter(it -> it == '\n').count();
-                result.append("Line count: ").append(lines).append('\n');
+                for (String path : findFiles(arg.content, recursive)) {
+                    String content = readFile(path);
+                    int lines = 1 + (int) content.chars().filter(it -> it == '\n').count();
+                    result.append(path).append(" 行数: ").append(lines).append('\n');
+                }
+            }
+        }
+        {
+            Arg arg = findArg('a');
+            if (arg != null) {
+                for (String path : findFiles(arg.content, recursive)) {
+                    String content = readFile(path);
+                    int codeline = 0, emptyline = 0, commentline = 0;
+                    String[] lines = content.split(System.getProperty("line.separator"));
+                    for (int i = 0 ; i < lines.length ; ++i) {
+                        if (lines[i].trim().length() <= 1) {
+                            emptyline += 1;
+                        } else if (lines[i].indexOf("//") >= 1) {
+                            commentline += 1;
+                        } else {
+                            codeline += 1;
+                        }
+                    }
+                    result.append(path).append(" 代码行/空行/注释行: ").append(codeline).append('/').append(emptyline).append('/').append(commentline).append('\n');
+                }
             }
         }
         {
@@ -84,7 +124,7 @@ public class WordCount {
         }
     }
 
-    private void parseArgs(String[] args) {
+    private void parseArgs(String[] args) { // 解析参数
         int state = 0;
         char readFlag = '\0';
         for (int i = 0; i < args.length; ++i) {
@@ -109,11 +149,32 @@ public class WordCount {
             argList.add(new Arg(readFlag, ""));
     }
 
-    private Arg findArg(char flag) {
+    private List<String> findFiles(String filter, boolean recursive) {
+        filter = filter.replace(".", "\\.").replace("*", ".*");
+        return findFiles("./", Pattern.compile(filter), recursive);
+    }
+
+    private List<String> findFiles(String basePath, Pattern filter, boolean recursive) {
+        List<String> ret = new ArrayList<>();
+        for (File f : new File(basePath).listFiles()) {
+            if (f.isDirectory()) {
+                if (recursive) {
+                    ret.addAll(findFiles(f.getAbsolutePath(), filter, true));
+                }
+            } else {
+                if (filter.matcher(f.toPath().getFileName().toString()).matches()) {
+                    ret.add(f.getPath().replace("./", ""));
+                }
+            }
+        }
+        return ret;
+    }
+
+    private Arg findArg(char flag) { // 搜索参数
         return argList.stream().filter(it -> it.flag == flag).findAny().orElse(null);
     }
 
-    private String readFile(String path) {
+    private String readFile(String path) { // 读文件
         try {
             byte[] encoded = Files.readAllBytes(Paths.get(path));
             return new String(encoded, "UTF-8");
